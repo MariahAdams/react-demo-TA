fa5ef1c9b11c109b89eb3cf7fe0eb848af16295f
MariahAdams <mariah.adams23@yahoo.com>
2019-02-19 10:28:24 -0800

    updating babel config

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
             cacheDirectory: true
           }
         }
